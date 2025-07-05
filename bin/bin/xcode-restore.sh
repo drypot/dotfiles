3c629a1108a030de286cb23ca1531769e776ffdc
drypot <drypot@gmail.com>
2025-07-05 17:28:18 +0900

    update

--- bin/bin/xcode-restore.sh
+++ bin/bin/xcode-restore.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 (
-
 cd ~/Downloads
 
 cp Library/Preferences/com.apple.dt.Xcode.plist ~/Library/Preferences/
@@ -11,5 +10,4 @@ cp -R Library/Developer/Xcode/UserData/KeyBindings ~/Library/Developer/Xcode/Use
 cp -R Library/Developer/Xcode/UserData/FontAndColorThemes ~/Library/Developer/Xcode/UserData/
 
 cp -R Library/Developer/Xcode/Templates ~/Library/Developer/Xcode/
-
 )
